0ad40918d26ff221a0e07ac8c40634865b68b06f
Lucas Fujiwara <lucastfujiwara@gmail.com>
2021-11-25 09:47:47 -0300

    feature: adiciona cors

--- src/web/main.ts
+++ src/web/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { WebModule } from './web.module';
+import { ConfigService } from '@nestjs/config';
+import { INestApplication } from '@nestjs/common';
+
+const setupCors = (app: INestApplication, config: ConfigService) => {
+  const origin = config.get<string>('CORS_ORIGINS')?.split(',') || [];
+
+  app.enableCors({
+    credentials: true,
+    origin,
+  });
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(WebModule);
+
+  setupCors(app, app.get(ConfigService));
+
   await app.listen(3333);
 }
 
